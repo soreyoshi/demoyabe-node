88c471f377be6b4369736107477afbd25c94e6c4
yoshiki yabe <soreyoshi@gmail.com>
2023-04-05 16:19:53 +0900

    m

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,10 +1,15 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+function add() {
     const a = 0;
-    for (var i = 0; i < 10000; i++) {
+    for (var i = 0; i < 1000; i++) {
         a += 1;
     }
-    await ontext.log(`count: ${a}`);
+    return a;
+}
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const a = add();
+    context.log(`count: ${a}`);
     context.res = {
         body: context.executionContext.invocationId
     };
