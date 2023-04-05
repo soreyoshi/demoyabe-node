61a076aa9ef0cbe0ae2e55c090dbd69c9e2ab38a
yoshiki yabe <soreyoshi@gmail.com>
2023-04-05 17:13:30 +0900

    m

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -2,14 +2,13 @@ async function add() {
     var a = 0;
     for (var i = 0; i < 1000; i++) {
         a += 1;
-    }
-    
+    };
     return a;
 }
 
 async function loga() {
     const a = add ();
-    context.log(`count: ${a}`)
+    context.log(`count: ${a}`);
     return a;
 }
 
@@ -17,7 +16,7 @@ module.exports = async function (context, req) {
     context.log('count 開始');
     const x = await loga();
     context.res = {
-        body: 'counted'
+        status: 200
     };
     context.log('count 終了');
 }
